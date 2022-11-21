09107e4414992a0df42f4df8e4cea48371f8fea3
Rebekah Taylor <rebekahtdesigns@gmail.com>
2022-11-21 07:26:11 -0800

    main

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, user! Guess the number.");
+            Console.Write("Guess number --->");
+            Console.ReadLine();
+            string UserAnswer = Console.ReadLine();
+            Console.Write(UserAnswer);
+        }
+    }
+}
